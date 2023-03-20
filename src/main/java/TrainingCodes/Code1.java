0d036428891da0866aa9db010683e82fb0a45de8
Ivanagimath <Ivanagimath@DESKTOP-03GBQ71>
2023-03-20 10:07:00 +0530

    Old Programs Added

---
+++ src/main/java/TrainingCodes/Code1.java
@@ -0,0 +1,34 @@
+package TrainingCodes;
+/**
+ * 
+ */
+
+/**
+ * @author Ivanagimath
+ *
+ */
+import java.util.Scanner;
+public class Code1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value of n:");
+		int n=sc.nextInt();
+		for(int i=2;i<=n;i++) {
+			for(int j=2;j<=n;j++) {
+				System.out.print("$ ");
+			}
+			for(int j=2;j<=n;j++) {
+				System.out.print("# ");
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
---
+++ src/main/java/TrainingCodes/Program1.java
@@ -0,0 +1,19 @@
+package TrainingCodes;
+/**
+ * 
+ */
+
+/**
+ * @author Ivanagimath
+ *
+ */
+public class Program1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println();
+	}
+
+}
